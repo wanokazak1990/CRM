a85624242d2acccd7cbe887df20c141267fd0ae4
andrewdeych <andrewdeych@gmail.com>
2018-12-27 12:38:17 +0300

    CRM: Дополнил проверки

--- resources/views/crm/worklist.blade.php
+++ resources/views/crm/worklist.blade.php
@@ -43,11 +43,31 @@
 					<tr>
 						<td>{{ date('d.m.Y', $traffic->action_date) }}</td>
 						<td>{{ date('H:i:s', $traffic->action_time) }}</td>
-						<td>{{ $traffic->traffic_type->name }}</td>
-						<td>{{ $traffic->assigned_action->name }}</td>
-						<td>{{ $traffic->client->name }}</td>
-						<td>{{ $traffic->model->name }}</td>
-						<td>{{ $traffic->manager->name }}</td>
+						<td>
+							@isset($traffic->traffic_type->name)
+								{{ $traffic->traffic_type->name }}
+							@endisset
+						</td>
+						<td>
+							@isset($traffic->assigned_action->name)
+								{{ $traffic->assigned_action->name }}
+							@endisset
+						</td>
+						<td>
+							@isset($traffic->client->name)
+								{{ $traffic->client->name }}
+							@endisset
+						</td>
+						<td>
+							@isset($traffic->model->name)
+								{{ $traffic->model->name }}
+							@endisset
+						</td>
+						<td>
+							@isset($traffic->manager->name)
+								{{ $traffic->manager->name }}
+							@endisset
+						</td>
 					</tr>
 					@endforeach
 				</tbody>
