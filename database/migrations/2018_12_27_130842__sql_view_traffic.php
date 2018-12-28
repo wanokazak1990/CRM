1e027d09704a38d267a844e419a1a357a914830c
Ivan <wanokazak1990@yandex.ru>
2018-12-28 09:26:54 +0300

    SQL VIEW TABS

---
+++ database/migrations/2018_12_27_130842__sql_view_traffic.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SqlViewTraffic extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $sql = "
+            CREATE VIEW _tab_traffics 
+            AS SELECT 
+                traffic.id,
+                traffic.creation_date,
+                traffic.traffic_type_id,
+                traffic.desired_model,
+                client.name,
+                /*client.lastname*/
+                traffic.comment,
+                traffic.manager_id,
+                traffic.admin_id,
+                traffic.assigned_action_id,
+                traffic.action_date,
+                traffic.action_time
+            FROM crm_traffics as traffic
+            JOIN crm_clients as client
+                on client.id = traffic.client_id
+        ";
+
+        DB::connection()->getPdo()->exec($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::connection()->getPdo()->exec("DROP VIEW _tab_traffics");
+    }
+}
