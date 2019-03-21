7ddaee36d25bb18d91c46eca3bf01ccd6b0efc30
Ivan <wanokazak1990@yandex.ru>
2019-03-21 16:01:28 +0300

    PRAVKI

--- app/crm_ofu_product.php
+++ app/crm_ofu_product.php
@@ -31,6 +31,11 @@ class crm_ofu_product extends Model
     		return self::createHtmlBlock();
     }
 
+    public static function sumProfit($worklistId)
+    {
+    	return self::where('worklist_id',$worklistId)->sum('profit');
+    }
+
     public static function createHtmlBlock($ofu_product = [])
     {
     	$users = user::pluck('name', 'id');
